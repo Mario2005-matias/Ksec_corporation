2ab23c042806a1c8ab75be74412974eb96e74308
matiasDev007 <mariomatias879@gmail.com>
2025-04-05 10:31:09 -0300

    terminei a main

--- src/app/service/page.tsx
+++ src/app/service/page.tsx
@@ -5,12 +5,12 @@ export default function Service(){
 
     return(
         <>
-          <main className='w-full h-72 bg-red-500 flex flex-row'>
-            <div id="conteudo">
-              <div>Produtos da Ksecurity_</div>
-              <h1>Mostra que você faz parte da comunidade em grande estilo</h1>
-              <h3>Só aqui você encontra produtos oficiais e exclusivos da nossa marca.</h3>
-              <div id="container-button">
+          <main className='w-full h-[80vh] flex flex-row justify-around items-center'>
+            <div className='w-[30rem]' id="conteudo">
+              <div className='w-[12rem] border-gray-400 border border-colapse rounded-md p-2 mb-5'>Produtos da Ksecurity_</div>
+              <h1 className="text-5xl">Mostra que você faz parte da comunidade em grande estilo</h1>
+              <h3 className='text-2xl mt-3 mb-3'>Só aqui você encontra produtos oficiais e exclusivos da nossa marca.</h3>
+              <div className="w-[12rem] font-bold text-sm bg-green-600 flex flex-row justify-evenly items-center p-2 rounded-md">
                 ESCOLHER PRODUTOS
               </div>
             </div>
