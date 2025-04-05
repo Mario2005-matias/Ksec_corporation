3f14ae49f222bbda1bc3e7f92104a653ef56b8ec
matiasDev007 <mariomatias879@gmail.com>
2025-04-05 09:39:49 -0300

    voltando para a minha branch

--- src/app/service/page.tsx
+++ src/app/service/page.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import Image from 'next/image'
 
 export default function Service(){
 
     return(
         <>
-          <h1>Carragando Servicos</h1>
+          <main className='w-full h-72 bg-red-500 flex flex-row'>
+            <div id="conteudo">
+              <div>Produtos da Ksecurity_</div>
+              <h1>Mostra que você faz parte da comunidade em grande estilo</h1>
+              <h3>Só aqui você encontra produtos oficiais e exclusivos da nossa marca.</h3>
+              <div id="container-button">
+                ESCOLHER PRODUTOS
+              </div>
+            </div>
+            <div id="img">
+              <Image src="/images/produtos/main.png" alt='Banner' width={500} height={1500} />
+            </div>
+          </main>
         </>
     )
 }
