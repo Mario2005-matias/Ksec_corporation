a11cf9e5102c812c3fd14ea4d20deb9866739aa6
dennisDev77 <diniscamela5@gmail.com>
2025-04-15 02:55:01 +0100

    refactor: 💡 Clean the component of page that no used

--- src/app/service/page.tsx
+++ src/app/service/page.tsx
@@ -1,37 +1,66 @@
 import React from "react";
-import Main from "./Main";
-import AfterMain from "./AfterMain";
+import NiveisDeSeguranca from "./NiveisDeSeguranca";
 import Projetos from "../home/Projetos";
-import OurServices from "./OurServices";
-import HowAsign from "./HowAsign";
+import ComoProtegemos from "./ComoProtegemos";
 import CallToAction from "./CallToAction";
 import FaqSection from "./FaqSection";
-//import Image from 'next/image'
+
 
 export default function Product() {
   return (
     <>
-      <Main />
-      <AfterMain />
-      <section className="Container w-full h-full flex flex-col justify-center items-center z-10">
-        <div className="flex flex-col items-center justify-center max-w-3xl mx-auto">
-          <h2 className="subTitle">Nossas Soluções</h2>
-          <p className="text-center text-zinc-400 font-lg">
-            Tecnologias avançadas para proteger sua empresa contra ameaças
-            digitais em constante evolução. Desenvolvemos ferramentas
-            inteligentes que monitoram, previnem e respondem a ataques
-            cibernéticos de forma eficaz.
-          </p>
-        </div>
+    <main className='h-full w-full'>
 
-        <div className="max-w-5xl">
-          <Projetos />
-        </div>
-      </section>
-      <OurServices/>
-      <HowAsign/>
-      <CallToAction/>
-      <FaqSection/>
+        <section className='h-full w-full flex flex-col justify-center items-center gap-4 mt-10 py-20'>
+           
+            <div className='max-w-5xl '>
+              <h2 className=' text-4xl text-zinc-200 font-bold text-center'>Saiba mais sobre os nossos Servicos</h2>
+              <p className='text-lg text-zinc-400 text-center font-light'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, non architecto? Consectetur sequi minima itaque quaerat libero porro dolor? Qui totam facilis laudantium facere sapiente voluptatum repudiandae dicta soluta sit.</p>
+            </div>
+          
+            <div className='max-w-5xl '>
+              <NiveisDeSeguranca />
+            </div>
+        </section>
+
+        <section className=" w-full h-full flex flex-col justify-center items-center gap-4 py-25 my-12">
+          <div className=" mx-auto">
+            <h2 className="subTitle">Nossas Soluções</h2>
+          </div>
+
+          <div className="max-w-5xl">
+            <Projetos />
+          </div>
+        </section>
+
+        <section className='w-full h-full py-24 flex flex-col items-center justify-center gap-4'>
+
+            <div className=''>        
+                <h2 className="subTitle" > Como protegemos seus dados </h2>
+            </div>
+
+            <div className=''>        
+                <ComoProtegemos/>
+            </div>
+        </section>
+
+
+          <section>
+              <CallToAction/>
+          </section>
+
+        <section className='w-full h-full  gap-4 py-24'> 
+
+          <div className='flex flex-col items-center justify-center'>
+               <h2 className='subTitle'> Perguntas Frequentes </h2>
+          </div> 
+
+          <div className=' '>
+             <FaqSection/>
+          </div>
+        </section>
+
+      </main>
     </>
   );
 }
